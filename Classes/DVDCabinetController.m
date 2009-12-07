06956d5e2659f59923b1f8487095058397d3b95f
beepscore <support@beepscore.com>
2009-12-06 17:29:07 -0800

    Ref Dudney sec 6.6

--- Classes/DVDCabinetController.m
+++ Classes/DVDCabinetController.m
@@ -107,9 +107,7 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     // Navigation logic may go here. Create and push another view controller.
-	// AnotherViewController *anotherViewController = [[AnotherViewController alloc] initWithNibName:@"AnotherView" bundle:nil];
-	// [self.navigationController pushViewController:anotherViewController];
-	// [anotherViewController release];
+	[self.navigationController popViewControllerAnimated:YES];
 }
 
 
